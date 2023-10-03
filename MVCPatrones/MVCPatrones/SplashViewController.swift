24900961480cfbb533cf64b3857b10611ffe3b9d
sergio97re <126094274+sergio97re@users.noreply.github.com>
2023-10-03 19:19:31 +0200

    Add files via upload

---
+++ MVCPatrones/MVCPatrones/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  MVCPatrones
+//
+//  Created by Sergio Reina Montes on 03/10/2023.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
